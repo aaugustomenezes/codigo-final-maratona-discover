cb834724245885f754ccc4fdd446b8da83b67a3e
aaugustomenezes <antonioaugustomenezes@gmail.com>
2021-08-17 04:48:51 -0300

    'Enter'

--- Projeto Financeiro/scripts.js
+++ Projeto Financeiro/scripts.js
@@ -110,10 +110,9 @@ const DOM = {
 const Utils = {
 
     formatAmount(value) {
-        //value = Number(value) * 100
-        value = Number(value.replace(/\,\./g, "")) * 100
-
-        return value
+        value = value * 100
+        
+        return Math.round(value)
     },
 
     formatDate(date) {
